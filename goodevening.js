2a5b35d44260aea2019355d5fc448c27dc2c2d00
eve-aps <131467510+eve-aps@users.noreply.github.com>
2024-03-23 18:05:04 +0000

    INDEX SIDEQUEST V1 + NEW LINKS
    index sidequest v1 leads to a copy of cube.html as a treat
    soundcloud and youtube links now present tooo

--- goodevening.js
+++ goodevening.js
@@ -1,3 +1,5 @@
+//site functionality
+
 function setNavbarUrls(){
     // Get the current domain URL
     const baseUrl = window.location.origin;
@@ -13,7 +15,66 @@ function goodEveningStartup(){
     setNavbarUrls();
 }
 
-// Call the function when the DOM content is loaded
+//gay shite
+
+// Anomaly event listener functs
+
+function addAnomalyEventListener(){
+    const anomaly = document.querySelector("#anomaly-gif");
+    anomaly.addEventListener("click",anomalyClicked);
+}
+
+function anomalyClicked(event){
+
+    const parentDiv = event.target.parentElement;
+
+    if (!parentDiv.querySelector(".sidequest-container")){
+        // Create a popup element
+        const popup = document.createElement('div');
+        popup.classList.add('sidequest-container');
+
+        // Add content to the popup
+        popup.innerHTML = `
+            <div class="sidequest-popup">
+                <p style="margin-bottom: 60px; margin-top: 10px;">Accept sidequest?</p>
+            </div>
+            <div class="sidequest-btns">
+                <button onclick="anomalySidequestAccept()" style="color: green; padding: 3px; margin: 3px; margin-right: 50px;">yes</button>
+                <button onclick="anomalySidequestDeny()" style="color: red; padding: 3px; margin: 3px; margin-left: 50px;">no</button>
+            </div>
+            `;
+
+        // Append the popup to the parent div
+        parentDiv.appendChild(popup);
+    }
+}
+
+function anomalySidequestAccept() {
+    // Testing
+    // alert('Sidequest accepted!');
+
+    // Remove popup
+    const popup = document.querySelector(".sidequest-container");
+    if (popup){
+        popup.remove();
+    }
+
+    // Reroute to cube etc ...
+    window.location.href = window.location.origin+"/cube.html";
+}
+
+function anomalySidequestDeny(){
+    // Remove popup
+    const popup = document.querySelector(".sidequest-container");
+    if (popup){
+        popup.remove();
+    }
+}
+
+// Main
+
+// Call the set up functs when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', function() {
     goodEveningStartup();
+    addAnomalyEventListener();
 });
