4e9f8377c5c03561eabeb16c1c9714c79a2f2bdb
goodevening <evecmurphy@gmail.com>
2025-04-19 15:09:10 +0100

    ported index.html (cube route needs creating)

--- wasteland/static/js/goodevening.js
+++ wasteland/static/js/goodevening.js
@@ -1,19 +1,70 @@
-function setNavbarUrls(){
-    // Get the current domain URL
-    const baseUrl = window.location.origin;
-
-    // Update the href attributes of the scrapyard and links links
-    document.getElementById('scrapyard-link').href = `${baseUrl}/templates/scrapyard.html`;
-    document.getElementById('links-link').href = `${baseUrl}/templates/links.html`;
-    // Blogs link
-}
+//site functionality
 
 // Startup function
 function goodEveningStartup(){
-    setNavbarUrls();
+    //posible database stuff?
 }
 
-// Call the function when the DOM content is loaded
+//gay shite
+
+// Anomaly event listener functs
+
+function addAnomalyEventListener(){
+    const anomaly = document.querySelector("#anomaly-gif");
+    anomaly.addEventListener("click",anomalyClicked);
+}
+
+function anomalyClicked(event){
+
+    const parentDiv = event.target.parentElement;
+
+    if (!parentDiv.querySelector(".sidequest-container")){
+        // Create a popup element
+        const popup = document.createElement('div');
+        popup.classList.add('sidequest-container');
+
+        // Add content to the popup
+        popup.innerHTML = `
+            <div class="sidequest-popup">
+                <p style="margin-bottom: 60px; margin-top: 10px;">Accept sidequest?</p>
+            </div>
+            <div class="sidequest-btns">
+                <button onclick="anomalySidequestAccept()" style="color: green; padding: 3px; margin: 3px; margin-right: 50px;">yes</button>
+                <button onclick="anomalySidequestDeny()" style="color: red; padding: 3px; margin: 3px; margin-left: 50px;">no</button>
+            </div>
+            `;
+
+        // Append the popup to the parent div
+        parentDiv.appendChild(popup);
+    }
+}
+
+function anomalySidequestAccept() {
+    // Testing
+    // alert('Sidequest accepted!');
+
+    // Remove popup
+    const popup = document.querySelector(".sidequest-container");
+    if (popup){
+        popup.remove();
+    }
+
+    // Reroute to cube etc ...
+    window.location.href = window.location.origin+"/cube.html";
+}
+
+function anomalySidequestDeny(){
+    // Remove popup
+    const popup = document.querySelector(".sidequest-container");
+    if (popup){
+        popup.remove();
+    }
+}
+
+// Main
+
+// Call the set up functs when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', function() {
-    goodEveningStartup();
+    //goodEveningStartup();
+    addAnomalyEventListener();
 });
